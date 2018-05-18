3dc2748eefde4d8e51b063ddafdf1544cfc2779d
Franz Hemmer <franz_hemmer@hotmail.com>
2018-05-17 22:12:21 -0500

    V1.0.0 -- First working version.

--- PXContainerMagic.lua
+++ PXContainerMagic.lua
@@ -1,6 +1,6 @@
 PXContainerMagic = {
   Name = "PXContainerMagic",
-  Version = "0.0.1",
+  Version = "1.0.0",
   LootInitiated = false
 }
 
@@ -11,52 +11,68 @@ function PXContainerMagic.OnAddOnLoaded(event, addonName)
   -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
   if addonName == PXContainerMagic.Name then
     d('PXCM -- OnAddOnLoaded')
-    PXContainerMagic:Initialize()
+    Initialize()
   end
-end
-
-function PXContainerMagic:Initialize()
-  d('PXCM -- Initialize')
-  ZO_CreateStringId("SI_BINDING_NAME_PX_CONTAINER_MAGIC_OPEN", "Open all containers")
 end
 
 function PXContainerMagic.OnLootUpdated()
   if (PXContainerMagic.LootInitiated == true) then
+    d('PXCM -- PXContainerMagic.OnLootUpdated()')
     LootAll()
     PXContainerMagic.LootInitiated = false
 
     local n = SCENE_MANAGER:GetCurrentScene().name
     if n == 'hudui' or n == 'interact' or n == 'hud' then
       SCENE_MANAGER:Show('hud')
-	else
+	  else
       SCENE_MANAGER:Show(n)
     end
+
+    zo_calllater(OpenContainers, 1000)
   end
+end
+
+function Initialize()
+  d('PXCM -- Initialize')
+  ZO_CreateStringId("SI_BINDING_NAME_PX_CONTAINER_MAGIC_OPEN", "Open all containers")
+end
+
+function OpenContainers()
+  local inventoryCount = GetBagSize(INVENTORY_BACKPACK)
+  d('PXCM -- PXContainerMagic:OpenContainers()')
+
+  for x = 0, inventoryCount do
+    local link = GetItemLink(INVENTORY_BACKPACK, x)
+    local itemType =  GetItemType(INVENTORY_BACKPACK, x)
+
+    if (itemType == ITEMTYPE_CONTAINER) then
+      d('PXCM -- Opening ' .. link)
+      openContainer(INVENTORY_BACKPACK, x)
+      break
+    end
+  end
+end
+
+function openContainer(bag, slot)
+  lastScene = SCENE_MANAGER:GetCurrentScene():GetName()
+  if lastScene == "interact" then
+    LootAll()
+    lastScene = "hudui"
+  end
+  
+  if IsProtectedFunction("UseItem") then
+    --d("Attempting to open container "..GetItemLink(bag, slot))
+    CallSecureProtected("UseItem", bag, slot)
+  else
+    UseItem(bag, slot)
+  end 
+  calledFromQuest = true
+  LootAll()
+  zo_callLater(OpenContainers, 1000)
 end
 
 ---------------------------------------------------------------------------------------------------------
 -- KEYBINDING EVENTS:
 ---------------------------------------------------------------------------------------------------------
-function PXContainerMagic_OpenContainers()
-  local inventoryCount = GetNumBagUsedSlots(INVENTORY_BACKPACK)
-  d('PXCM -- PXContainerMagic_OpenContainers()')
-
-  for x = 1, inventoryCount do
-    local link = GetItemLink(INVENTORY_BACKPACK, x)
-    local itemType =  GetItemType(INVENTORY_BACKPACK, x)
-
-    if (itemType == ITEMTYPE_CONTAINER) then
-      if IsProtectedFunction("UseItem") then
-        d("PXCM -- Attempting to open container " .. link)
-        CallSecureProtected("UseItem", INVENTORY_BACKPACK, x)
-        PXContainerMagic.LootInitiated = true
-        --zo_callLater(function() LootAll(true) end, 300)
-      else
-        UseItem(INVENTORY_BACKPACK, x)
-      end 
-    end
-  end
-end
-
 EVENT_MANAGER:RegisterForEvent(PXContainerMagic.Name, EVENT_ADD_ON_LOADED, PXContainerMagic.OnAddOnLoaded)
 EVENT_MANAGER:RegisterForEvent(PXContainerMagic.name, EVENT_LOOT_UPDATED , PXContainerMagic.OnLootUpdated)
